baa2e1410d171351bbca791898e7189443e3be77
Aryan <aryan@gmail.com>
2026-07-07 23:45:00 +0530

    updated files

--- api/index.py
+++ api/index.py
@@ -1,7 +1,9 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workofolio.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "workofolio.settings"
+)
 
 application = get_wsgi_application()
